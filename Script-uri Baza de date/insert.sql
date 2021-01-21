2d4aa368910ada04e615dbbaad106508419e6904
Antighin Alexandru <73889652+alexantighin@users.noreply.github.com>
2021-01-21 23:39:04 +0200

    Add files via upload

---
+++ Script-uri Baza de date/insert.sql
@@ -0,0 +1,58 @@
+INSERT INTO categorii (nume_categorie) VALUES ('Pizza');
+INSERT INTO categorii (nume_categorie) VALUES ('Burgeri');
+INSERT INTO categorii (nume_categorie) VALUES ('Paste');
+INSERT INTO categorii (nume_categorie) VALUES ('Salate');
+INSERT INTO categorii (nume_categorie) VALUES ('Supe');
+INSERT INTO categorii (nume_categorie) VALUES ('Deserturi');
+INSERT INTO categorii (nume_categorie) VALUES ('Bauturi');
+
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values('Pizza Al Tono',29.40,'blat, ulei de masline, salsa di pomodoro, mozzarella, ton, masline, ceapa, 850g','pizza/pizza-al-tono.png',1);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values('Pizza Superdeluxe',29.70,'blat, ulei de masline, salsa di pomodoro, extra mozzarella, salam, sunca, cabanos, ciuperci, ceapa, ardei gras, porumb, 850g','pizza/pizza-superdeluxe.png',1);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values('Pizza Deluxe',29.70,'blat, ulei de masline, salsa di pomodoro, mozzarella, salam, sunca, ciuperci proaspete, ceapa, ardei gras, masline feliate, 850g','pizza/pizza-deluxe.png',1);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values('Pizza Meat Deluxe',29.70,'blat, ulei de masline, salsa di pomodoro, mozzarella, cabanos, sunca presata, piept de pui, extra salam, 850g','pizza/pizza-meat-deluxe.png',1);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values('Pizza Gurmand',29.70,'blat, ulei de masline, mozzarella, salsa di pomodoro, salam, sunca presata, kaizer, carnati ardelenesti, 850g','pizza/pizza-gurmand.png',1);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values('Pizza Capriciosa',30.40,'blat, ulei de masline, salsa di pomodoro, mozzarella, cabanos, ton, ceapa, rosii, lamaie, 850g','pizza/pizza-capriciosa.png',1);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values('Pizza Salami',28.63,'blat, ulei de masline, salsa di pomodoro, mozzarella, extra salam uscat, 850g','pizza/pizza-salami.png',1);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values('Pizza Prosciutto',37.61,'blat, ulei de masline, salsa di pomodoro, extra mozzarella, gorgonzola, prosciutto crudo, 850g','pizza/pizza-prosciutto.png',1);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values('Pizza Formaggi',35.37,'blat, ulei de masline, salsa di pomodoro, extra mozzarella, cascaval, gorgonzola, parmezan, mascarpone, 850g','pizza/pizza-formaggi.png',1);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values('Pizza Fresca',28.63,'blat, ulei de masline, salsa di pomodoro, mozzarella, gorgonzola, salam uscat, piept de pui, rosii, 850g','pizza/pizza-fresca.png',1);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values('Pizza Prosciutto Funghi',28.63,'blat, ulei de masline, salsa di pomodoro, extra mozzarella, sunca, ciuperci cu usturoi, 850g','pizza/pizza-prosciutto-funghi.png',1);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values('Pizza Picante',28.63,'blat, ulei de masline, mozzarella, salsa di pomodoro, chorizo, 850g','pizza/pizza-picante.png',1);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values('Pizza Bacon',29.70,'blat, ulei de masline, salsa di pomodoro, mozzarella, sunca presata, ciuperci proaspete, ceapa rosie, 850g','pizza/pizza-bacon.png',1);
+
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Manly',61.97,'branza cheddar, bacon, ceapa, sos','burgeri/manly.png',2);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Cali',55.69,'salata, rosii, ceapa, mustar, castraveti, unt','burgeri/cali.png',2);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('The Original Burger',68.38,'ceapa, branza, mustar, muraturi, salata, rosii','burgeri/the_original_burger.png',2);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Ultimate Bacon',59.83,'bacon, branza cheddar, ceapa, salata, unt','burgeri/ultimate_bacon.png',2);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Vegan BBQ',51.19,'mustar, salsa, bbq, sos, salata coleslaw, jalapeno','burgeri/vegan_bbq.png',2);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Umami',57.70,'parmesan frico, ciuperci shiitake, rosii, ceapa caramelizata, sos','burgeri/umami.png',2);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Classic Cheeseburger',51.29,'sos, branza, salata, ceapa, rosii','burgeri/classic_cheeseburger.png',2);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Throwback',55.56,'branza alba cheddar, mustar, sos, castraveti, ceapa','burgeri/throwback.png',2);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Trufflemaker',55.43,'mustar, salsa, trufe','burgeri/trufflemaker.png',2);
+
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Paste Cu Fructe De Mare',44.9,'paste, creveti decortificati, calamar, creveti black tiger, carne scoici, busuioc, verdeata, rosii, usturoi','paste/paste_specialitati_fructe_mare_big.png',3);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Penne Cu Ton',29.7,'penne, ton in ulei, rosii decojite, patrunjel, ardei iute, usturoi, busuioc','paste/penne-cu-ton-dopo-poco.png',3);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Penne Quattro Formaggi',28.91,'penne, smantana, gorgonzola, parmezan, mozzarella, branza Brie','paste/paste_penne_quatro_formaggi_big.png',3);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Spaghetti Carbonara',24.90,'spaghetti, ou, smantana, parmezan, bacon','paste/paste_spagheti_carbonara_big.png',3);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Spaghetti Cu Scoici',30.10,'spaghetti, scoici, usturoi, rosii, busuioc, vin alb, verdeata, ardei iute','paste/spaghetti-cu-scoici.png',3);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Spaghetti Torinesse',28.92,'spaghetti, file de vita, sos de rosii, ciuperci, usturoi, busuioc','paste/spaghetti-torinesse.png',3);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Tagliatelle Cu Ragu De Vita',25.40,'tagliatelle, carne ragu de vita, smantana, parmezan, verdeata','paste/paste_tagliatelle_bologneze_big.png',3);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Tagliatelle Cu Somon Afumat',28.90,'tagliatelle, somon fume, smantana','paste/paste_tagliatelle_somon_afumat_big.png',3);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Tortelini Cu Sunca',28.90,'paste umplute, parmezan, sunca, smantana','paste/paste_tortelini_sunca_smantana_big.png',3);
+
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Salata Caesar',24.44,'salata verde, crutoane, anchois, iaurt, piept de pui grill, focaccia','salate/salate_caesar_big.png',4);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Salata Cu Piept De Pui',19.23,'rosii, castraveti, salata verde, iaurt, piept de pui grill, porumb, ardei gras, marar, focaccia','salate/salate_piept_pui_big.png',4);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Salata Cu Ton',27.51,'salata verde, ton, rosii, castraveti, ou, ardei gras, avocado, focaccia','salate/salate_ton_big.png',4);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Salata Greceasca',19.23,'rosii, castraveti, ardei gras, ceapa, branza feta, masline, oregano, focaccia','salate/salate_greceasca_pui_big.png',4);
+
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Ciorba De Burta',14.90,'supa de vita, burta de vita, smantana, oua, legume, ceapa, telina, focaccia','supe/ciorba_de_burta_big.png',5);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Ciorba De Vacuta',14.90,'pulpa de vita, morcov, telina, ceapa, mazare, ardei gras, focaccia','supe/ciorba_de_vacuta_big.png',5);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Supa De Pui',13.90,'piept de pui, telina, morcov, ceapa, pastarnac, patrunjel, taitei de casa, focaccia','supe/ciorbe_supa_pui_big.png',5);
+
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Coca Cola',7,'1,25L','bauturi/coca-cola-1-7179.png',7);
+INSERT into produse (nume_produs, pret_produs, descriere_produs, imagine_produs, id_categorie) values ('Dorna Apa Plata',5,'2L','bauturi/Dorna.png',7);
+
+
+
+INSERT INTO utilizatori (user_utilizator,email_utilizator, parola_utilizator) VALUES ('admin', 'admin@admin.com', 'admin');
+INSERT INTO detalii_utilizatori (nume_utilizator,prenume_utilizator,adresa_utilizator,telefon_utilizator,admin_utilizator, id_utilizator) VALUES ('Nume Admin', 'Prenume Admin1', 'Adresa Admin', '0700000000',true, 1);
